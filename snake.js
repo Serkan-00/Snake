7ef057e459b13ba29de5f8cde66fd2eb3b7dbd68
serkancoskun01@hotmail.com <serkancoskun01@hotmail.com>
2023-09-08 17:08:50 +0100

    extended snake size

--- snake.js
+++ snake.js
@@ -1,5 +1,10 @@
 export const SNAKE_SPEED = 1
-const snakeBody = [{ x: 11, y: 11}]
+const snakeBody = [
+    { x: 10, y: 11},
+    { x: 11, y: 11},
+    { x: 12, y: 11}
+
+]
 
 export function update() { 
  console.log('update Snake')
