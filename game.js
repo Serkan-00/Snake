491e4b8b4f8154a4a7725678c6a1120e089d4e10
serkancoskun01@hotmail.com <serkancoskun01@hotmail.com>
2023-09-08 16:55:54 +0100

    snake time

--- game.js
+++ game.js
@@ -1,5 +1,8 @@
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED } from './snake.js'
+
+
 let lastRenderTime = 0 
-const SNAKE_SPEED = 1 
+
 
 
 function main(currentTime) { 
@@ -7,8 +10,22 @@ function main(currentTime) {
   const secondSinceLastRender = (currentTime - lastRenderTime) / 1000 
   if (secondSinceLastRender < 1 / SNAKE_SPEED) return 
 
-  console.log('Render')
   lastRenderTime = currentTime
+
+  update()
+  draw()
 }
 
 window.requestAnimationFrame(main)
+
+function update() { 
+  updateSnake()
+
+
+}
+
+function draw() { 
+  drawSnake()
+
+  
+}
---
+++ snake.js
@@ -0,0 +1,9 @@
+export const SNAKE_SPEED = 1
+
+export function update() { 
+ console.log('update Snake')
+}
+
+export function draw() { 
+  console.log('draw Snake')
+}
