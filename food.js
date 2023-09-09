f7510f9104bd58f4dc4f1bd86f0b135e4f6ad3bd
serkancoskun01@hotmail.com <serkancoskun01@hotmail.com>
2023-09-09 14:32:13 +0100

    food can be eaten

--- food.js
+++ food.js
@@ -1,6 +1,13 @@
-let food = { x: 0, y: 0 }
+import { onSnake, expandSnake } from './snake.js'
+
+let food = getRandomFoodPosition
+const EXPANSION_RATE = 1 
 
 export function update() { 
+    if (onSnake(food)) { 
+        expandSnake(EXPANSION_RATE)
+        food = getRandomFoodPosition
+    }
     
 }
    
@@ -11,4 +18,12 @@ export function draw(gameBoard) {
       foodElement.classList.add('food')
       gameBoard.appendChild(foodElement) 
      
+}
+
+function getRandomFoodPosition() { 
+    let newFoodPosition 
+    while (newFoodPosition == null || onSnake(newFoodPosition)) { 
+        newFoodPosition = randomGridPosition() 
+    }
+    return newFoodPosition
 }
